212c6b8bdb3dbfaad0d7b2e2a944a32bc2edaa60
SYLCN05 <Siyaryalcin25@gmail.com>
2026-01-04 19:49:32 +0100

    Movie entity for database table and seeding

---
+++ MovieApp/MovieApp.Server/Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieApp.Server.Models
+{
+    [Table("Movies")]
+    public class Movie
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Series_Title { get; set; }
+        public int Released_Year { get; set; }
+        public string Certificate {  get; set; }
+        public string Runtime { get; set; }
+        public string Genre  { get; set; }
+        public int IMDB_Rating { get; set; }
+        public string Overview { get; set; }
+        public int Meta_score  { get; set; }
+        public string Director { get; set; }
+        public string Star1 { get; set; }
+        public string Star2 { get; set; }
+        public string Star3 { get; set; }
+        public string Star4 { get; set; }
+        public int No_Of_Votes { get; set; }
+        public decimal Gross {  get; set; }
+
+    }
+}
