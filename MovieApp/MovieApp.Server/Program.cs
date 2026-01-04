d44ed9902439973e5444a57b5c4d8b9411a75994
SYLCN05 <Siyaryalcin25@gmail.com>
2026-01-04 22:01:51 +0100

    add swagger to backend for api endpoint test

--- MovieApp/MovieApp.Server/Program.cs
+++ MovieApp/MovieApp.Server/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MovieApp.Server.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    //var fileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    //options.IncludeXmlComments(System.IO.Path.Combine(
+    //    AppContext.BaseDirectory, fileName)
+    //    );
+
+    options.EnableAnnotations();
+
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "MovieApp",
+        Version = "v1",
+        Description = "This is the swagger ui to call the movie endpoints during development"
+    });
+});
 
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
     options.UseMySQL(
@@ -35,6 +53,13 @@ app.MapStaticAssets();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
@@ -43,6 +68,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.MapFallbackToFile("/index.html");
+//app.MapFallbackToFile("/index.html");
 
 app.Run();
