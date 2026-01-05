97084e03ae333963b0cde0178a650ebccb2b23fc
SYLCN05 <Siyaryalcin25@gmail.com>
2026-01-05 21:55:48 +0100

    Basic movie GET method

---
+++ MovieApp/MovieApp.Server/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieApp.Server.Models;
+
+namespace MovieApp.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class MovieController : Controller
+    {
+        private ApplicationDBContext _context;
+        public MovieController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("GetMovies")]
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            return await _context.Movies.ToListAsync();
+        }
+    }
+}
