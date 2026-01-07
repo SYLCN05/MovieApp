79d7b62e329f06666500d0453beffc5c0ace5c22
SYLCN05 <Siyaryalcin25@gmail.com>
2026-01-07 23:56:42 +0100

    Add account controller login method returns jwt token for auth

--- MovieApp/MovieApp.Server/Controllers/AccountController.cs
+++ MovieApp/MovieApp.Server/Controllers/AccountController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using MovieApp.Server.DTO;
 using MovieApp.Server.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace MovieApp.Server.Controllers
 {
@@ -12,12 +15,13 @@ namespace MovieApp.Server.Controllers
         private ApplicationDBContext _context;
         private UserManager<MovieUser> _userManager;
         private RoleManager<IdentityRole> _roleManager;
-
-        public AccountController(ApplicationDBContext context, UserManager<MovieUser> userManager, RoleManager<IdentityRole> roleManager)
+        private readonly IConfiguration _configuration;
+        public AccountController(ApplicationDBContext context, UserManager<MovieUser> userManager, RoleManager<IdentityRole> roleManager,IConfiguration configuration)
         {
             _context = context;
             _userManager = userManager;
             _roleManager = roleManager;
+            _configuration = configuration;
         }
 
         [HttpPost("RegisterUser")]
@@ -42,5 +46,48 @@ namespace MovieApp.Server.Controllers
             }
             return BadRequest("User can't be created");
         }
+
+        [HttpPost("Login")]
+        private async Task<ActionResult> Login(LoginUserDTO loginInput)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var user = await _userManager.FindByNameAsync(loginInput.Username);
+
+                    if (user == null || !await _userManager.CheckPasswordAsync(user, loginInput.Password))
+                    {
+                        throw new Exception("Invalid login attempt!");
+                    }
+                    else
+                    {
+                        var signingCredentials = new SigningCredentials(
+                            new SymmetricSecurityKey(
+                                System.Text.Encoding.UTF8.GetBytes(_configuration["JWT:Key"])),
+                                SecurityAlgorithms.HmacSha256
+                                );
+
+                        var claims = new List<Claim>();
+
+                        claims.Add(new Claim(ClaimTypes.Name, user.UserName));
+                        claims.AddRange((await _userManager.GetRolesAsync(user)).Select(r => new Claim(ClaimTypes.Role, r)));
+
+                        var jwtObject = new JwtSecurityToken(
+                            issuer: _configuration["JWT:Issuer"],
+                            audience: _configuration["JWT:Audience"],
+                            claims: claims,
+                            expires: DateTime.Now.AddMinutes(60),
+                            signingCredentials: signingCredentials);
+
+                        var jwtString = new JwtSecurityTokenHandler()
+                            .WriteToken(jwtObject);
+
+                        return StatusCode(StatusCodes.Status200OK, jwtString);
+                    }
+                }
+            }
+        }
+
     }
 }
---
+++ MovieApp/MovieApp.Server/DTO/LoginUserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieApp.Server.DTO
+{
+    public class LoginUserDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
